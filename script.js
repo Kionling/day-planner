220fe6d7d4272f39bc18830b7ff4b66a9c05eacd
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-24 20:13:12 -0700

    Past text area color function is functional.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
 $("#currentDay").text(moment());
+var textAreas = $("textarea")
+console.log(textAreas)
+function presentTime(){
+    if (textAreas !== moment().hour()){
+        $("textarea").addClass("past")
+    }
+}
 
-
-
+presentTime();
 
 
 
