f6924004f3421e22937aa025e5d0c789f124c989
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-24 23:03:32 -0700

    All color indicators for future, present, past are functional

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 $(document).ready(function () {
-$("#currentDay").text(moment());
-var textAreas = $("textarea")
-console.log(textAreas)
-function presentTime(){
-    if (textAreas !== moment().hour()){
-        $("textarea").addClass("past")
-    }
+
+$("#currentDay").text(moment().format( "dddd, MMMM Do YYYY, h:mm:ss a" ));
+      var uniTime = moment().hour();
+var oneTry = $("#9amInput").attr("value2");
+// alert(oneTry);
+alert(uniTime);
+if(uniTime === oneTry){
+    $("#9amInput").addClass("present")
+}
+if(uniTime > oneTry){
+    $("#9amInput").addClass("past")
+}
+if(uniTime < oneTry){
+    $("#9amInput").addClass("future")
 }
 
-presentTime();
+var ar=document.getElementsByTagName("textarea");
+
+
+for(var i=0; i< ar.length; i++){
+
+    if(uniTime === ar[i].name * 1){
+        ar[i].setAttribute("class", "present");
+    }
+    if(uniTime > ar[i].name * 1){
+        ar[i].setAttribute("class", "future");
+    }
+    if(uniTime < ar[i].name * 1){
+        ar[i].setAttribute("class", "past");
+    }
 
 
 
 
 
+}
+      
+    
 
 
 
 
 
+  })
 
-
-
-
-
-
-
-})
+      
+    
+    
+    
