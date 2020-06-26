6dd5ff31520470905aec400157d0368f825087c1
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-25 17:55:56 -0700

    Created functional code for saving day planner inputs. Updating readME as well

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# day-planner
-Day Planner
+# Day Planner
+Moment.js is a very useful library of time that allows one to have an alternative to the Javascript Date Object directly. Here, the objective is to create time-orientated, color change events to time-blocks. These times blocks include an hour display, a textarea input, and a savebutton. The save button saves the user input, and when refreshed the data is still there, saved in the local storage. 
+## Site Picture
+![Day planner](Assets/Dayplanner.png)
+## Installing
+Since we're using Jquery, Moment.js they both have external libraries that will enable their functionality locally. 
+**Here are the provided links to those libraries in the HTML**
+```
+    <head>
+        <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    </head>
+    .
+    code here
+    .
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+
+```
+This is necessary for proper use of all the Jquery specific objects, operators, etc.
+## Built With 
+- HTML
+- CSS
+- JQuery
+# Deployed Link
+
+ - [Link to Github repository]()
+ - [Link to Site]()
+
--- script.js
+++ script.js
@@ -1,40 +1,67 @@
-$(document).ready(function () {
 
-$("#currentDay").text(moment().format( "dddd, MMMM Do YYYY, h:mm:ss a" ));
-      var uniTime = moment().hour();
-var oneTry = $("#9amInput").attr("value2");
-// alert(oneTry);
-alert(uniTime);
-if(uniTime === oneTry){
-    $("#9amInput").addClass("present")
-}
-if(uniTime > oneTry){
-    $("#9amInput").addClass("past")
-}
-if(uniTime < oneTry){
-    $("#9amInput").addClass("future")
-}
-
-var ar=document.getElementsByTagName("textarea");
-
-
-for(var i=0; i< ar.length; i++){
-
-    if(uniTime === ar[i].name * 1){
-        ar[i].setAttribute("class", "present");
+    $(document).ready(function() {
+    
+        $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+        uniTime = moment().hour();
+    
+        ar = $("textarea");
+        for (var i = 0; i < ar.length; i++) {
+            if (uniTime === ar[i].name * 1) {
+                ar[i].setAttribute("class", "present");
+            }
+            if (uniTime > ar[i].name * 1) {
+                ar[i].setAttribute("class", "past");
+            }
+            if (uniTime < ar[i].name * 1) {
+                ar[i].setAttribute("class", "future");
+            }
+            var key = ar[i].id;
+            var msg = localStorage.getItem(key);
+            ar[i].value = msg;
+        }
+    })
+    
+    function saveEv(e) {
+        var eltx = $("#" + e);
+        var tmsg = eltx.val();
+        localStorage.setItem(e, tmsg);
     }
-    if(uniTime > ar[i].name * 1){
-        ar[i].setAttribute("class", "future");
-    }
-    if(uniTime < ar[i].name * 1){
-        ar[i].setAttribute("class", "past");
-    }
+// $("#currentDay").text(moment().format( "dddd, MMMM Do YYYY, h:mm:ss a" ));
+//       var uniTime = moment().hour();
+// // var oneTry = $("#9amInput").attr("value2");
+// // // alert(oneTry);
+// // // alert(uniTime);
+// // if(uniTime === oneTry){
+// //     $("#9amInput").addClass("present")
+// // }
+// // if(uniTime > oneTry){
+// //     $("#9amInput").addClass("past")
+// // }
+// // if(uniTime < oneTry){
+// //     $("#9amInput").addClass("future")
+// // }
+
+// var ar=document.getElementsByTagName("textarea");
 
 
 
+// for(var i=0; i< ar.length; i++){
+
+//     if(uniTime === ar[i].name * 1){
+//         ar[i].setAttribute("class", "present");
+//     }
+//     if(uniTime > ar[i].name * 1){
+//         ar[i].setAttribute("class", "past");
+//     }
+//     if(uniTime < ar[i].name * 1){
+//         ar[i].setAttribute("class", "future");
+//     }
 
 
-}
+// }
+
+
+
       
     
 
@@ -42,7 +69,6 @@ for(var i=0; i< ar.length; i++){
 
 
 
-  })
 
       
     
